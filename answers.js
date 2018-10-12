ac4596e81c68c8d43c8a9f3b1a6fd27befa1b9d9
Kippy92 <39587941+Kippy92@users.noreply.github.com>
2018-10-12 10:11:56 -0400

    Add files via upload

--- answers.js
+++ answers.js
@@ -75,7 +75,7 @@
 	//Counting, the Dojo Way
 	for(var i = 1; i <= 100; i++) {
   		if ((i % 5 == 0) && (i % 10 == 0)) {
-    		console.log("Coding Dojo")
+    		console.log("Coding Dojo");
   		}
   		else if (i % 5 == 0) {
     		console.log("Coding");
